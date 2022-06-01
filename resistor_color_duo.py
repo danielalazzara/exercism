ce635e1822f644b205d2856ee8dc2c032d10dfe1
danielalazzara <97309237+danielalazzara@users.noreply.github.com>
2022-06-01 14:33:08 +0100

    Create  resistor_color_duo.py

---
+++ resistor_color_duo.py
@@ -0,0 +1,4 @@
+def value(colors):
+    color = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white'] 
+    return color.index(colors[0])*10 + color.index(colors[1])
+    
