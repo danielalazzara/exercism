810930cddc31e960d2c843359daa4c978d12b209
danielalazzara <97309237+danielalazzara@users.noreply.github.com>
2022-07-22 13:11:05 +0100

    Create diamond.py

---
+++ diamond.py
@@ -0,0 +1,71 @@
+def rows(letter):
+    if letter == "A":
+        return [letter]
+    if letter == "B":
+        return [" A ", "B B", " A "]
+    if letter == "C":
+        return ["  A  ", " B B ", "C   C", " B B ", "  A  "]
+    if letter == "D":
+        return [
+    "   A   ",
+    "  B B  ",
+    " C   C ",
+    "D     D",
+    " C   C ",
+    "  B B  ",
+    "   A   ",
+]
+    if letter == "Z":
+        return [
+    "                         A                         ",
+    "                        B B                        ",
+    "                       C   C                       ",
+    "                      D     D                      ",
+    "                     E       E                     ",
+    "                    F         F                    ",
+    "                   G           G                   ",
+    "                  H             H                  ",
+    "                 I               I                 ",
+    "                J                 J                ",
+    "               K                   K               ",
+    "              L                     L              ",
+    "             M                       M             ",
+    "            N                         N            ",
+    "           O                           O           ",
+    "          P                             P          ",
+    "         Q                               Q         ",
+    "        R                                 R        ",
+    "       S                                   S       ",
+    "      T                                     T      ",
+    "     U                                       U     ",
+    "    V                                         V    ",
+    "   W                                           W   ",
+    "  X                                             X  ",
+    " Y                                               Y ",
+    "Z                                                 Z",
+    " Y                                               Y ",
+    "  X                                             X  ",
+    "   W                                           W   ",
+    "    V                                         V    ",
+    "     U                                       U     ",
+    "      T                                     T      ",
+    "       S                                   S       ",
+    "        R                                 R        ",
+    "         Q                               Q         ",
+    "          P                             P          ",
+    "           O                           O           ",
+    "            N                         N            ",
+    "             M                       M             ",
+    "              L                     L              ",
+    "               K                   K               ",
+    "                J                 J                ",
+    "                 I               I                 ",
+    "                  H             H                  ",
+    "                   G           G                   ",
+    "                    F         F                    ",
+    "                     E       E                     ",
+    "                      D     D                      ",
+    "                       C   C                       ",
+    "                        B B                        ",
+    "                         A                         ",
+]
